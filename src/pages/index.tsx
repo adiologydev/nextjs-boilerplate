304a35b6d8b6b6fab8c5a4972e362b54c6e57dc4
Aditya N. Tripathi <adityatripathidelhi@gmail.com>
2023-01-03 12:16:11 +0530

    init: base

---
+++ src/pages/index.tsx
@@ -0,0 +1,5 @@
+const Index = () => {
+  return <>Boilerplate</>;
+};
+
+export default Index;
---
+++ src/utils/AppConfig.ts
@@ -0,0 +1,6 @@
+export const AppConfig = {
+  site_name: 'Boilerplate',
+  title: 'Nextjs Boilerplate',
+  description: 'Starter code for your Nextjs Boilerplate with Tailwind CSS',
+  locale: 'en',
+};
